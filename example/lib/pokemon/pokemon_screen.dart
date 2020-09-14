9e00e1a5a14201fc8df334be480fd96e45ec7688
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-09-14 12:09:46 -0300

    update example todo

--- example/lib/pokemon/pokemon_screen.dart
+++ example/lib/pokemon/pokemon_screen.dart
@@ -4,52 +4,52 @@ import 'package:examplecube/pokemon/repository/model/pokemon.dart';
 import 'package:examplecube/pokemon/widgets/pokemon_item_widget.dart';
 import 'package:flutter/material.dart';
 
-class PokemonScreen extends StatelessWidget {
+class PokemonScreen extends CubeWidget<PokemonCube> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
   @override
-  Widget build(BuildContext context) {
-    return CubeBuilder<PokemonCube>(
-      onError: (cube, text) {
-        scaffoldKey?.currentState?.showSnackBar(SnackBar(content: Text(text)));
-      },
-      builder: (context, cube) {
-        return Scaffold(
-          key: scaffoldKey,
-          appBar: AppBar(
-            title: 'Pokemon'.title(context, color: Colors.white),
-          ),
-          body: Stack(
-            children: <Widget>[
-              cube.list.build<List<Pokemon>>(
-                (data) {
-                  return ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      if (index >= data.length - 3) {
-                        cube.loadList(isMore: true);
-                      }
-                      return PokemonItemWidget(
-                        item: data[index],
-                        onClick: (item) {},
-                      );
-                    },
+  Widget buildView(BuildContext context, PokemonCube cube) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: 'Pokemons with CubeWidget'.title(context, color: Colors.white),
+      ),
+      body: Stack(
+        children: <Widget>[
+          cube.list.build<List<Pokemon>>(
+            (data) {
+              return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  if (index >= data.length - 3) {
+                    cube.loadList(isMore: true);
+                  }
+                  return PokemonItemWidget(
+                    item: data[index],
+                    onClick: (item) {},
                   );
                 },
-              ),
-              cube.progress.build<bool>(
-                (value) {
-                  return value
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : SizedBox.shrink();
-                },
-                animate: true,
-              ),
-            ],
+              );
+            },
           ),
-        );
-      },
+          cube.progress.build<bool>(
+            (value) {
+              return value
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : SizedBox.shrink();
+            },
+            animate: true,
+          ),
+        ],
+      ),
     );
+  }
+
+  @override
+  void onError(BuildContext context, PokemonCube cube, String text) {
+    scaffoldKey?.currentState?.showSnackBar(SnackBar(content: Text(text)));
+    super.onError(context, cube, text);
   }
 }

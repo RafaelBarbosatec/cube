59c123ebd5c27eb79e557968c3bc9119a47a882f
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-10-04 19:26:35 -0300

    adiciona pareametros replace in getString

--- lib/src/localization/strings_location.dart
+++ lib/src/localization/strings_location.dart
@@ -11,11 +11,13 @@ class StringsLocation {
 
   StringsLocation._internal();
 
-  String getString(String key) {
-    return _myLocalizations.trans(key);
+  String getString(String key, {Map<String, String> params}) {
+    String str = _myLocalizations.trans(key);
+    params?.forEach((key, value) => str = str.replaceAll(key, value));
+    return str;
   }
 }
 
-String getString(String key) {
-  return StringsLocation.instance.getString(key);
+String getString(String key, [Map<String, String> params]) {
+  return StringsLocation.instance.getString(key, params: params);
 }

054b61d5efd03ee32796ea2c84f12e12032fbdfd
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2020-11-10 18:39:11 -0300

    update example

--- example/lib/todo/todo_cube.dart
+++ example/lib/todo/todo_cube.dart
@@ -3,11 +3,7 @@ import 'package:cubes/cubes.dart';
 class TodoCube extends Cube {
   final todoList = ObservableList<String>(value: ['first TODO', 'second TODO']);
 
-  void addTodo(String todo) {
-    todoList.add(todo);
-  }
+  void addTodo(String todo) => todoList.add(todo);
 
-  void delete(String todo) {
-    todoList.remove(todo);
-  }
+  void delete(String todo) => todoList.remove(todo);
 }

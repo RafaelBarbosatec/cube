ae4f5f9bf34143a61289381fdf793e9023f5083c
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2021-02-16 14:10:29 -0300

    inpromement in location

--- lib/src/localization/cubes_localization_delegate.dart
+++ lib/src/localization/cubes_localization_delegate.dart
@@ -6,21 +6,27 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'cubes_localization.dart';
 
+/// Delegate responsible for internationalization from json files
 class CubesLocalizationDelegate extends LocalizationsDelegate<CubesLocalization> {
+  /// primary constructor of the CubesLocalizationDelegate
   const CubesLocalizationDelegate(
     this.supportedLocations, {
     this.pathFiles = 'lang/',
   })  : assert(supportedLocations != null),
         assert(supportedLocations.length > 0);
 
+  /// path where the json files are located
   final String pathFiles;
+
+  /// List of the locations supported
   final List<Locale> supportedLocations;
 
   @override
   bool isSupported(Locale locale) {
-    return true;
+    return supportedLocations.map((e) => e.languageCode).contains(locale.languageCode);
   }
 
+  /// Get delegates to run internationalization
   List<LocalizationsDelegate> get delegates => [
         this,
         DefaultCupertinoLocalizations.delegate,

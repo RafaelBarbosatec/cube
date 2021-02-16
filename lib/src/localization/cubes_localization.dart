dd3f4f19b78f4604dca47193756ffd2f5cb387cb
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2021-02-16 14:31:57 -0300

    adiciona alguns comentarios

--- lib/src/localization/cubes_localization.dart
+++ lib/src/localization/cubes_localization.dart
@@ -19,14 +19,13 @@ class CubesLocalization {
   /// path where the json files are located
   final String pathFiles;
 
-  Map<String, String> _sentences;
+  final Map<String, String> _sentences = {};
 
   /// Load json according to locale
   Future<bool> load() async {
     var data = await rootBundle.loadString('$pathFiles${locale.languageCode}.json');
     Map<String, dynamic> _result = json.decode(data);
-
-    _sentences = {};
+    _sentences.clear();
     _result.forEach((key, dynamic value) {
       _sentences[key] = value.toString();
     });

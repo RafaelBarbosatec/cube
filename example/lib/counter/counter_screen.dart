945117613d6d5a44ca3838a1cacf3cb9ecc80925
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2021-03-15 09:29:33 -0300

    ajustes

--- example/lib/counter/counter_screen.dart
+++ example/lib/counter/counter_screen.dart
@@ -2,39 +2,40 @@ import 'package:cubes/cubes.dart';
 import 'package:examplecube/counter/counter_cube.dart';
 import 'package:flutter/material.dart';
 
-class CounterScreen extends CubeWidget<CounterCube> {
+class CounterScreen extends StatelessWidget {
   @visibleForTesting
   static const KEY_FLOATING_BUTTON = Key('CounterScreen.KEY_FLOATING_BUTTON');
 
   @override
-  Widget buildView(BuildContext context, CounterCube cube) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Cubes.getString('counter')),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(Cubes.getString('description_counter')),
-            cube.count.build<int>(
-              (value) => Text(value.toString()),
+  Widget build(BuildContext context) {
+    return CubeBuilder<CounterCube>(
+      onAction: (cube, action) {
+        print(action);
+      },
+      builder: (BuildContext context, CounterCube cube) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(Cubes.getString('counter')),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(Cubes.getString('description_counter')),
+                cube.count.build<int>(
+                  (value) => Text(value.toString()),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        key: KEY_FLOATING_BUTTON,
-        onPressed: cube.increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          floatingActionButton: FloatingActionButton(
+            key: KEY_FLOATING_BUTTON,
+            onPressed: cube.increment,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
-  }
-
-  @override
-  void onAction(BuildContext context, CounterCube cube, CubeAction action) {
-    print(action);
-    super.onAction(context, cube, action);
   }
 }

65dd8f3a250ce1c7d8e3210d1b961c8fffcb584b
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2021-03-25 14:06:16 -0300

    adiciona novos testes unitarios

--- test/observable/observable_list_test.dart
+++ test/observable/observable_list_test.dart
@@ -2,62 +2,62 @@ import 'package:cubes/cubes.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  ObservableList<int> _observableValue;
+  ObservableList<int> _observableListValue;
   List<int> _valueNotify;
   setUp(() {
-    _observableValue = ObservableList(value: []);
-    _observableValue.addListener(() {
-      _valueNotify = _observableValue.value;
+    _observableListValue = ObservableList(value: []);
+    _observableListValue.addListener(() {
+      _valueNotify = _observableListValue.value;
     });
   });
 
   tearDown(() {
     _valueNotify = null;
-    _observableValue.dispose();
+    _observableListValue.dispose();
   });
 
   test('verify initial value', () {
-    expect(_observableValue.value, []);
+    expect(_observableListValue.value, []);
   });
 
   test('verify update list', () {
-    _observableValue.update([3, 2, 1]);
+    _observableListValue.update([3, 2, 1]);
     expect(_valueNotify, [3, 2, 1]);
   });
 
   test('verify add value in list', () {
-    _observableValue.add(1);
+    _observableListValue.add(1);
     expect(_valueNotify, [1]);
-    _observableValue.add(3);
+    _observableListValue.add(3);
     expect(_valueNotify, [1, 3]);
   });
 
   test('verify addAll in list', () {
-    _observableValue.addAll([3, 2, 1]);
+    _observableListValue.addAll([3, 2, 1]);
     expect(_valueNotify, [3, 2, 1]);
   });
 
   test('verify remove value in list', () {
-    _observableValue.update([3, 2, 1]);
-    _observableValue.remove(1);
+    _observableListValue.update([3, 2, 1]);
+    _observableListValue.remove(1);
     expect(_valueNotify, [3, 2]);
   });
 
   test('verify clean list', () {
-    _observableValue.update([3, 2, 1]);
+    _observableListValue.update([3, 2, 1]);
     expect(_valueNotify, [3, 2, 1]);
-    _observableValue.clear();
+    _observableListValue.clear();
     expect(_valueNotify, []);
   });
 
   test('verify modifyitem', () {
-    _observableValue.update([3, 2, 1]);
+    _observableListValue.update([3, 2, 1]);
     expect(_valueNotify, [3, 2, 1]);
-    _observableValue.modifyItem(0, (value) => 5);
+    _observableListValue.modifyItem(0, (value) => 5);
     expect(_valueNotify, [5, 2, 1]);
-    _observableValue.removeAt(0);
+    _observableListValue.removeAt(0);
     expect(_valueNotify, [2, 1]);
-    _observableValue.modifyItem(0, (value) => 5);
+    _observableListValue.modifyItem(0, (value) => 5);
     expect(_valueNotify, [5, 1]);
   });
 }

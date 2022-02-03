b3f901128ee12b3371e43f40e1e70985e712c362
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2022-02-03 12:42:28 -0300

    remove unnecessary imports

--- lib/src/localization/cubes_localization_delegate.dart
+++ lib/src/localization/cubes_localization_delegate.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'cubes_localization.dart';
